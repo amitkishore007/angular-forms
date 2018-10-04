566e12307949bbeed2a20c1219df954cbfdb643c
Amit Kishore <amit.kishore@biz2credit.org>
2018-10-04 17:29:24 +0530

    completed template forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,10 +6,60 @@ import { NgForm } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   
-  onSubmit(signupForm: NgForm) {
-    console.log(signupForm);
+  @ViewChild('signupForm') signupForm: NgForm;
+
+  user = {
+      name: '',
+      email: '',
+      secret: '' 
+  };
+  
+  formSubmitted:boolean = false;
+
+  defaultName: string = 'amitkishore';
+
+  onSubmit() {
+    this.user.name = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secret = this.signupForm.value.secret;
+    this.formSubmitted = true;
+    console.log(this.signupForm);
   }
+
+  onSuggestUsername() {
+    //setvalue() require to set the value for the whole form inputs
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: 'Akm@123',
+    //     email: 'kishoreamit5@gmail.com'
+    //   },
+    //   secret: 'pet'
+    // });
+
+
+    //to set value for specific input user
+    // form.patchvalue()
+
+    this.signupForm.form.patchValue({
+      userData:  {
+        username: 'Amitkishore007'
+      }
+    });
+    
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+    console.log(this.signupForm);
+  }
+
+  onResetForm() {
+    this.formSubmitted = false;
+    this.signupForm.reset();
+  }
+
 
 }
