d0d9d323277141a90a9b1033350e73a3396048c7
Amit Kishore <amit.kishore@biz2credit.org>
2018-10-05 11:00:57 +0530

    completed reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,58 +8,22 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent implements OnInit{
   
-  @ViewChild('signupForm') signupForm: NgForm;
+  genders = ['male', 'female'];
 
-  user = {
-      name: '',
-      email: '',
-      secret: '' 
-  };
-  
-  formSubmitted:boolean = false;
-
-  defaultName: string = 'amitkishore';
-
-  onSubmit() {
-    this.user.name = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.secret = this.signupForm.value.secret;
-    this.formSubmitted = true;
-    console.log(this.signupForm);
-  }
-
-  onSuggestUsername() {
-    //setvalue() require to set the value for the whole form inputs
-    // this.signupForm.setValue({
-    //   userData: {
-    //     username: 'Akm@123',
-    //     email: 'kishoreamit5@gmail.com'
-    //   },
-    //   secret: 'pet'
-    // });
+  signupForm: FormGroup;
 
 
-    //to set value for specific input user
-    // form.patchvalue()
-
-    this.signupForm.form.patchValue({
-      userData:  {
-        username: 'Amitkishore007'
-      }
-    });
-    
-  }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null,Validators.required),
+      'email': new FormControl(null,[Validators.required, Validators.email]),
+      'gender': new FormControl(null,[Validators.required])
+    });
+  }
+
+  onSubmit() {
     console.log(this.signupForm);
   }
-
-  onResetForm() {
-    this.formSubmitted = false;
-    this.signupForm.reset();
-  }
-
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
@@ -11,7 +11,7 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
